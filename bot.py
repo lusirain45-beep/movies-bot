9bb238a597510ee4ddc76f63891d53458b35660b
lusirain45-beep <lusirain45@gmail.com>
2026-06-12 14:36:46 +0300

    fix gemini model

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ SUBDL_KEY = os.environ.get("SUBDL_KEY")
 
 bot = telebot.TeleBot(TOKEN)
 genai.configure(api_key=GEMINI_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 search_results = {}
 user_mode = {}  # تتبع وضع المستخدم
